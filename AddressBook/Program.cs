cfead1694daa7e4a7d5331b37ea1e4fb64b8aa6d
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-17 10:47:12 +0530

    [Prasanna] Add: Implemented the contact details

---
+++ AddressBook/Program.cs
@@ -0,0 +1,11 @@
+﻿using AddressBookProblem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Address book Problem :");
+        AddressBook book = new AddressBook();
+        book.CreateContact();
+    }
+}
