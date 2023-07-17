2652ad3ca864606d4c425dee57e01628df36844b
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-17 11:46:44 +0530

    [Prasanna] Add: Implemented the Edit contact option

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -26,5 +26,45 @@ namespace AddressBookProblem
             Console.WriteLine(contact.FirstName + "\n " + contact.LastName + "\n " + contact.Address + "\n " + contact.City + "\n " + contact.State + "\n " + contact.Zip + "\n " + contact.PhoneNumber + "\n " + contact.Email);
             addressBook.Add(contact);
         }
+        public void EditContact(string name)
+        {
+            foreach(var contact in addressBook)
+            {
+                if (contact.FirstName.Equals(name) || contact.LastName.Equals(name) || contact.Address.Equals(name) || contact.City.Equals(name) || contact.State.Equals(name) || contact.Zip.Equals(name) || contact.PhoneNumber.Equals(name) || contact.Email.Equals(name))
+                    Console.WriteLine("Enter the Option to Edit\n1.Last Name\n2.Address\n3.city\n4.state\n5.zip\n6.phone number \n7.Email");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+
+                {
+                    case 1: contact.LastName = Console.ReadLine();
+                        break;
+                    case 2:
+                        contact.Address = Console.ReadLine();
+                        break;
+                    case 3:
+                        contact.City = Console.ReadLine();
+                        break;
+                    case 4:
+                        contact.State = Console.ReadLine();
+                        break;
+                    case 5:
+                        contact.Zip = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        contact.PhoneNumber = Convert.ToInt64( Console.ReadLine());
+                        break;
+                    case 7:
+                        contact.Email = Console.ReadLine();
+                        break;
+                }
+            }
+        }
+        public void Display()
+        {
+            foreach (var data in addressBook)
+            {
+                Console.WriteLine("First name --->"+data.FirstName+"\nLast Name ----->"+data.LastName);
+            }
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AddressBookProblem;
+using System.Text;
 
 internal class Program
 {
@@ -6,6 +7,30 @@ internal class Program
     {
         Console.WriteLine("Welcome to Address book Problem :");
         AddressBook book = new AddressBook();
-        book.CreateContact();
+
+        bool flag = true;
+        while(flag)
+        {
+            Console.WriteLine( "1.Create Contact \n2.Edit Contact\n3.Display\n4.Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    book.CreateContact();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the  First name");
+                    string fname = Console.ReadLine();
+                    book.EditContact(fname);
+                    break;
+                case 3:
+                    book.Display();
+                    break;
+                case 4:
+                    flag = false;
+                    break;
+            }
+        }
+           
     }
 }
